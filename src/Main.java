563a27dba0a38253ee0d8c0b26936a02efe3b514
Alexandre Cabana - École <cabanaA@educ.college-montreal.qc.ca>
2024-08-23 11:08:43 -0400

    creaton function

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        String mot = "banane";
-        String wordguest = "";
-        for (int i =0; i<mot.length(); i++){
-            wordguest += "_";
-        }
-        int nd = 0;
-        int ndmax = 3;
-        while (nd < ndmax) {
-            nd++;
-            System.out.println("Entre la lettre choisi ou le mot final");
-            System.out.println(wordguest);
-            String userinput;
-            userinput = scanner.nextLine();
-            if (userinput.equals(mot)) {
-                System.out.println("Bravo! Vous avez deviné le mot en "+nd+" essaies. Le mot était "+ mot);
+        String word = "banane";
+        String wordguest = GenerateHiddenWord(word.length());
+        int numberofguess = 0;
+        int maxguess = 4;
+        while (numberofguess < maxguess) {
+            System.out.println("Le mot actuel est: "+wordguest);
+            String userinput = AskUserWord(scanner);
+            if (PlayerWin(word,userinput)) {
+                Victory(numberofguess, maxguess, word);
                 break;
             }
-            else if (mot.contains(userinput)) {
-                System.out.println("La lettre que vous avez choisis est dans le mot secret. Vous êtes à " +nd+" essaies sur "+ndmax + ".");
-                String newwordguest = "";
-                for (int i=0;i<mot.length(); i++){
-                    if (mot.charAt(i) == userinput.charAt(0)){
-                        newwordguest += userinput;
-                    } else if (wordguest.charAt(i) != '_'){
-                        newwordguest += mot.charAt(i);
-                    } else{
-                        newwordguest += "_";
-                    }
-                }
-                nd--;
-                wordguest = newwordguest;
-                if (wordguest.equals(mot)){
-                    System.out.println("Bravo! Vous avez deviné le mot en "+nd+" essaies. Le mot était "+ mot);
+            else if (word.contains(userinput)) {
+                wordguest = AddLetterToHiddenWord(word, userinput, wordguest);
+                if (PlayerWin(word, wordguest)){
+                    Victory(numberofguess, maxguess, word);
                     System.exit(1);
+                }
+                else {
+                    numberofguess = WrongGuess(numberofguess, maxguess);
                 }
             }
             else{
-                System.out.println("Wrong Guest");
+                numberofguess = WrongGuess(numberofguess, maxguess);
             }
         }
+    }
+    public static String AskUserWord(Scanner scanner){
+        System.out.println("Entre la lettre choisi ou le mot final");
+        return scanner.nextLine();
+    }
+    public static String GenerateHiddenWord(int lenofword){
+        String hiddenword = "";
+        for (int i =0; i<lenofword; i++){
+            hiddenword += "_";
+        }
+        return hiddenword;
+    }
+    public static boolean PlayerWin(String reponse, String userinput){
+        return reponse.equals(userinput);
+    }
+    public static void Victory(int numberofguess, int numberofguessmax, String word){
+        System.out.println("Bravo! Vous avez deviné le mot. Vous êtes à " + numberofguess +" essaies sur "+numberofguessmax + ". Le mot était "+ word);
+    }
+    public static String AddLetterToHiddenWord(String word, String letter, String CurrentGuessWord){
+        String response = "";
+        for (int i=0;i<word.length(); i++){
+            if (word.charAt(i) == letter.charAt(0)){
+                response += letter.charAt(0);
+            } else if (CurrentGuessWord.charAt(i) != '_'){
+                response += word.charAt(i);
+            } else{
+                response += "_";
+            }
+        }
+        return response;
+    }
+    public static int WrongGuess(int guesscount, int guessmaxtry){
+        guesscount++;
+        System.out.println("La lettre que vous avez choisis est dans le mot secret. Vous êtes à " + guesscount +" essaies sur "+ guessmaxtry + ".");
+        return guesscount;
     }
 }
